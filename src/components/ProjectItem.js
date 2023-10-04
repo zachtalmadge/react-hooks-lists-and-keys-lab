430dbc5de7e75502e3106e9cdb26ef12cc8937e2
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-03 21:50:54 -0400

    pass tests

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,13 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {
+          technologies.map(tech => {
+            return (
+              <span>{tech}</span>
+            )
+          })
+        }
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,21 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {
+          projects.map(project => {
+            let { id, name, about, technologies} = project
+            return (
+              <ProjectItem 
+                key={ id } 
+                name={ name } 
+                about={ about } 
+                technologies={ technologies }
+              />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
